55f8ea596ba30c310e29ae81d805113b1894c136
Prosenjit Manna <prosenjit@itobuz.com>
2018-10-16 13:26:26 +0530

    product sort directive added

--- src/app/shared-components/product-sort.directive.ts
+++ src/app/shared-components/product-sort.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, HostListener } from '@angular/core';
+import { Router  } from '@angular/router';
 import { ActionSheetController } from '@ionic/angular';
 
 @Directive({
@@ -10,40 +11,51 @@ export class ProductSortDirective {
     console.log('tst');
     const actionSheet =  await this.actionSheetController.create({
       header: 'Sort by',
-      buttons: [{
-        text: 'Sort by popularity',
-        role: 'destructive',
-        handler: () => {
-          console.log('popularity clicked');
-        }
-      }, {
-        text: 'Sort by avarage rating',
-        handler: () => {
-          console.log('rating clicked');
-        }
-      }, {
+      buttons: [
+      //   {
+      //   text: 'Sort by popularity',
+      //   role: 'destructive',
+      //   handler: () => {
+      //     console.log('popularity clicked');
+      //   }
+      // },
+      //  {
+      //   text: 'Sort by avarage rating',
+      //   handler: () => {
+      //     console.log('rating clicked');
+      //   }
+      // },
+       {
         text: 'Sort by new arival',
         handler: () => {
-          console.log('new arival clicked');
+          this.redirect('date');
         }
-      }, {
-        text: 'Sort by low to high',
-        handler: () => {
-          console.log('1 clicked');
-        }
-      , {
-        text: 'Sort by high to low',
-        handler: () => {
-          console.log('2 clicked');
-        }
-      }]
+      },
+       {
+      //   text: 'Sort by low to high',
+      //   handler: () => {
+      //     this.redirect('price');
+      //   }
+      // },
+      //  {
+      //   text: 'Sort by high to low',
+      //   handler: () => {
+      //     this.redirect('price-desc');
+      //   }
+      // }
+    ]
     });
    await actionSheet.present();
   }
 
   constructor(
-    public actionSheetController: ActionSheetController
+    private actionSheetController: ActionSheetController,
+    private router: Router
   ) { }
+
+  redirect(val) {
+    this.router.navigate(['/products'], {queryParams: {orderby: val}});
+  }
 
 }
 
